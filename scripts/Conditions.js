468ecfcfde1553623ff851fd03aba95bca6880a9
tillDGN <119479499+tillDGN@users.noreply.github.com>
2022-11-30 19:39:35 +0100

    cleanups

--- scripts/Conditions.js
+++ scripts/Conditions.js
@@ -2,7 +2,7 @@ class Conditions
 {
     #faecherwahlManager
     #conditions = {}
-        ;
+
     constructor(faecherwahlManager)
     {
         this.#faecherwahlManager = faecherwahlManager;
@@ -18,43 +18,42 @@ class Conditions
     }
 }
 
-
 function hatNaWiB(faecher)
 {
-    faecher = faecher.filter(fach => isNaWi(fach));
+    const nawi = faecher.filter(fach => isNaWi(fach));
 
-    return numberFromNiveau(faecher[0].niveau) >= numberFromNiveau('B');
+    return fulfillsNiveaus(nawi, ['B']);
 }
 
 function hatEinWahlfach(faecher)
 {
-    //console.log(faecher);
-    const b = faecher.some(fach => fach.type === "wahl");
-    //console.log(b);
-    return b;
+    return faecher.some(fach => fach.type === "wahl");
 }
 
 function matheHatGenugNawi(faecher)
 {
-    nawi = faecher.filter(fach => isNaWi(fach));
+    const nawi = faecher.filter(fach => isNaWi(fach));
 
-    return
-        fulfillsNiveaus(nawi, ['A', 'B', 'C']) ||
+    return fulfillsNiveaus(nawi, ['A', 'B', 'C']) ||
         fulfillsNiveaus(nawi, ['B', 'B', 'B']) ||
         fulfillsNiveaus(nawi, ['B', 'B', 'C', 'C']);
 }
 
 function fulfillsNiveaus(faecher, requiredNiveaus)
 {
-    if (faecher.length < requiredNiveaus.length) return false;
+    const requireds = requiredNiveaus.map(numberFromNiveau);
+    const niveaus = faecher.map(fach => numberFromNiveau(fach.niveau));
 
-    requiredNiveaus = requiredNiveaus.map(numberFromNiveau);
-    const niveaus = faecher.map(fach => numberFromNiveau(fach.niveau)).splice(0, requiredNiveaus.length);
+    return fulfillsNiveaus_impl(niveaus, requireds);
+}
 
-    console.log(niveaus);
-    console.log(requiredNiveaus);
+function fulfillsNiveaus_impl(niveaus, requireds)
+{
+    if (niveaus.length < requireds.length) return false;
 
-    const fulfills = niveaus.map((niveau, i) => niveau >= requiredNiveaus[i]);
-
-    return fulfills.reduce((acc, current) => acc && current, true);
+    for (let i = 0; i < requireds.length; i++)
+    {
+        if (niveaus[i] < requireds[i]) return false;
+    }
+    return true;
 }
